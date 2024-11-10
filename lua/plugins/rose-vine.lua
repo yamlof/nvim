b814c278c71354127cd9254b87f4841430cabd72
Edwin <hhgsxdesktop@dh206190.halls.liv.ac.uk>
2024-11-10 20:39:40 +0000

    nvim dotfiles

---
+++ lua/plugins/rose-vine.lua
@@ -0,0 +1,20 @@
+return {
+  "rose-pine/neovim",
+  lazy = false,
+  name = "rose-pine",
+  config = function()
+    vim.cmd.colorscheme "rose-pine"
+  end
+}
+
+
+--require("rose-pine").setup({
+ -- variant = "main",
+ -- styles = {
+  --  transparency = true
+ -- }
+
+--}
+--
+--)
+--vim.cmd.colorscheme "rose-pine"
---
+++ lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.wo.number =true
+vim.cmd('highlight Normal ctermbg=none')
+vim.cmd('highlight Normal guibg=none')
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.wo.relativenumber = true
